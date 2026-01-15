0e48649d6f83e56a0c1e69f8aa4589795e486ddd
vhanok70 <vhanok70@gmail.com>
2026-01-15 18:28:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("app").innerHTML = `
+  <div class="header">Zenipay</div>
+  <div class="card">Wallet Balance: ₹0</div>
+  <div class="card">Send • Receive • History</div>
+`;
